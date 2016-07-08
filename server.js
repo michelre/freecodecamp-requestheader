041a7d676e128450e9870c65ca0bdbf490773caa
Rémi Michel <remi.michel@kaizen-solutions.net>
2016-07-08 11:15:27 +0200

    fix ipaddress

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/whoami', (req, res) => {
-  console.log(req.headers, req.connection.remoteAddress)
   res.send({
     language: utils.parseLanguage(req.headers['accept-language']),
-    ipaddress: req.connection.remoteAddress,
+    ipaddress: utils.parseIpAddress(req.connection.remoteAddress),
     software: utils.parseUserAgent(req.headers['user-agent']),
   });
 });
--- utils.js
+++ utils.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   parseLanguage: (langHeaders) => langHeaders.split(',')[0],
-  parseUserAgent: (header) => /\(([^\)]+)\)/g.exec(header)[1]
+  parseUserAgent: (header) => /\(([^\)]+)\)/g.exec(header)[1],
+  parseIpAddress: (header) => /((?:[0-9]{1,3}\.){3}[0-9]{1,3})/.exec(header)[1],
 };
